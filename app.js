4a725231dea31611f2b63dfeaaf00385bd3f2833
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-14 21:53:30 +0100

    Show movie title as a list on the page

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require("express");
 var request = require("request");
 
 var app = express();
+app.set("view engine", "ejs");
 
 app.get("/results", function(req, res){
   request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", function(error, response, body){
     if(!error && response.statusCode === 200) {
 
-      var results = JSON.parse(body);
-      res.send(results["Search"][0]);
+      var data = JSON.parse(body);
+      res.render("results", { data: data})
     }
   })
 })
