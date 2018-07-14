4269406bbd6f16bd988655fa01f915dc84dbf4bc
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-14 19:38:05 +0100

    Parse JSON data

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var app = express();
 app.get("/results", function(req, res){
   request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", function(error, response, body){
     if(!error && response.statusCode === 200) {
-      res.send(body);
+
+      var results = JSON.parse(body);
+      res.send(results["Search"][0]);
     }
   })
 })
