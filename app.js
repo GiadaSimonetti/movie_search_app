f54baef7272786fa6680b093e34a5aaba6508372
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-14 19:10:37 +0100

    Send API body on the page

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var request = require('request');
+var express = require("express");
+var request = require("request");
 
 var app = express();
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.get("/results", function(req, res){
+  request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", function(error, response, body){
+    if(!error && response.statusCode === 200) {
+      res.send(body);
+    }
+  })
+})
+
+app.listen(3000, function(){
   console.log("Movie app has stearted!!!");
 });
