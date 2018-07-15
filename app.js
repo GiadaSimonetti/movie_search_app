f6185bfdff966f7200ffe150974317d79ba9f2ed
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-15 10:35:20 +0100

    Create a search view and add search functionality to app.js

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ var request = require("request");
 var app = express();
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+  res.render("search")
+});
+
 app.get("/results", function(req, res){
-  request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", function(error, response, body){
+  var query = req.query.search;
+  var url = `http://www.omdbapi.com/?s=${query}&apikey=thewdb`
+
+  request(url, function(error, response, body){
     if(!error && response.statusCode === 200) {
 
       var data = JSON.parse(body);
       res.render("results", { data: data})
     }
-  })
-})
+  });
+});
+
+// http://www.omdbapi.com/?i=tt3896198&apikey=thewdb
+// &apikey=thewdb
 
 app.listen(3000, function(){
   console.log("Movie app has stearted!!!");
